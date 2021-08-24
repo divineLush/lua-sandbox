127eeea422897f34be9031f1a28add115429ccfd
alex <alex@debian-BULLSEYE-live-builder-AMD64>
2021-08-24 23:46:55 +0700

    strings

---
+++ strings.lua
@@ -0,0 +1,7 @@
+quote = 'long long long long long long long long long long long long long long long long long'
+
+print(string.len(quote), '\n')
+print(string.gsub(quote, 'ong', 'ua'), '\n')
+print(string.find(quote, ' long'), '\n')
+print(string.upper(quote), '\n')
+print(string.lower('LUA'), '\n')
